17e4f7c3844bb7593551edab9bdd286a5fe630de
Stefan_mit_Spck_Editor <backebackekuchen16@gmail.com>
2024-11-07 02:25:28 +0100

    ConvexGeometry() aus Beispiel10()

--- QWASNEU/ANF.js
+++ QWASNEU/ANF.js
@@ -9,30 +9,63 @@ let FFF = [1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 3, 2, 1, 1, 2, 3, 2, 1, 1, 
 
 //∨
 
-function ANF(Obj) {
+function ANF(OBJ) {
   
-  Obj.ANF=[1111];
+  OBJ.ANF=[
+  	 [1,1,1,1,1,1,3,0],
+  	 [1,1,1,1,1,1,0,3],
+  	 ];
   }
 
-function alle_Punkte(Obj) {
+function alle_Punkte(OBJ) {
 	 let PL=[];
-	 let EL=OBJ_X[0];
+	 let EL=OBJ[0];
 	 let xyz=[];
+	 let dr=0;
 	 for (let i=0;i<EL.length;i++) 
 	   for (let j=0;j<i;j++) 
-	     for (let k=0;k<j;k++) 
-	       PL.push([DREIEBENEN(EL[i],EL[j],EL[k]),[]]);
+	     for (let k=0;k<j;k++) {
+	     	 dr=DREIEBENEN(EL[i], EL[j], EL[k]);
+	       if (dr[3]==1) PL.push([dr,[]]);
+	       }
 	 for (let i=0;i<PL.length;i++)
-  	 for (let j=0;j<EL.length;j++) PL[i][1].push(TFIND(OBJ_X,j,PL[i][0]));
+  	 for (let j=0;j<EL.length;j++) PL[i][1].push(TFIND(OBJ,j,PL[i][0]));
   
-  Obj.alle_Punkte=PL;
+  OBJ.PL=PL;
   }
+
+function alle_konvexen_Teile(OBJ) {
+	 let RET=[];
+	 let RET0=[];
+	 let flag=true;
+	 let drin=0;
+	 for (let c=0;c<OBJ.ANF.length;c++) {
+	   RET[c]=[];
+	   for (let i=0;i<OBJ.PL.length;i++) {
+	   	 flag=true;
+	   	 for (let j=0;j<OBJ.ANF[c].length;j++) {
+	   	 	 drin=TFIND(OBJ,j,OBJ.PL[i][0]);
+	   	 	 if (drin!=OBJ.ANF[c][j]&&OBJ.ANF[c][j]>0&&drin!=2) flag=false;
+  	   	 }
+	   	 
+  	   if (flag) RET[c].push(OBJ.PL[i][0]);
+	     }
+	   }
+	 OBJ.konv=RET;
+  }
+
+
+
+
 
 
 //---------------
 function ANF_Test() {
   ANF(OBJ_X);
   alle_Punkte(OBJ_X);
-  //console.log(TFIND(OBJ_X,);
-  console.error(JSON.stringify(OBJ_X.alle_Punkte.slice(0,2)));
+  //console.log(TFIND(OBJ_X,0,);
+  alle_konvexen_Teile(OBJ_X);
+  exportiere.innerHTML='cpoints='+JSON.stringify(OBJ_X.konv)+"\n";
+  selectText("exportiere");
+  console.error(JSON.stringify(OBJ_X.PL.slice(0,2))+'#'+JSON.stringify(OBJ_X.konv));
   }
